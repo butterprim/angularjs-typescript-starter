9bac78d250e9f598206fb81b3e66919f14d81b31
butterprim <lizettejoan@gmail.com>
2020-01-08 16:27:54 +0800

    Create directive without isolated scope

--- directives/click-logger/click-logger.directive.ts
+++ directives/click-logger/click-logger.directive.ts
@@ -1,11 +1,8 @@
 export const ClickLoggerDirective = {
   restrict: 'A',
-  scope: {
-    message: '<'
-  },
   link: function(scope, element, attrs, controllers) {
     element.on('click', function(event) {
-      console.log(scope.message);
+      console.log(attrs.bpClickLogger);
     });
   }
 };
